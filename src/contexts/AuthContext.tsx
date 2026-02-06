5d6ce7b1e73dda12e2e03480e841925b9e7432a6
flamencowerkstattapp <flamencowerkstatapp@gmail.com>
2026-02-06 21:29:00 +0100

    fix: set handleCodeInApp to false to avoid unauthorized-continue-uri error

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -146,7 +146,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     try {
       const actionCodeSettings: ActionCodeSettings = {
         url: `${window.location.origin}/?verified=true`,
-        handleCodeInApp: true,
+        handleCodeInApp: false,
       };
       
       await sendEmailVerification(userCredential.user, actionCodeSettings);
